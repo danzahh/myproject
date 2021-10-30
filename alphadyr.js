86b3e894858f7b25af11fe01c65a648357ae5026
danzahh <danielltaylor095@gmail.com>
2021-10-30 18:06:12 +0100

    first commit

---
+++ alphadyr.js
@@ -0,0 +1,62 @@
+function toggle(){
+    var x = snowmarker
+    document.getElementById("snowmarker");
+    if (snowmarker.style.display === "none") {
+        snowmarker.style.display = "block";
+    } else {
+        snowmarker.style.display = "none";
+    }
+}
+
+
+function toggle1(){
+    var x = tigermarker
+    document.getElementById("tigermarker");
+    if (tigermarker.style.display === "none") {
+        tigermarker.style.display = "block";
+    } else {
+        tigermarker.style.display = "none";
+    }
+}
+
+
+function toggle2(){
+    var x = pandamarker
+    document.getElementById("pandamarker");
+    if (pandamarker.style.display === "none") {
+        pandamarker.style.display = "block";
+    } else {
+        pandamarker.style.display = "none";
+    }
+}
+
+function toggle3(){
+    var x = orangutanmarker
+    document.getElementById("orangutanmarker");
+    if (orangutanmarker.style.display === "none") {
+        orangutanmarker.style.display = "block";
+    } else {
+        orangutanmarker.style.display = "none";
+    }
+}
+
+
+function toggle4(){
+    var x = lemurmarker
+    document.getElementById("lemurmarker");
+    if (lemurmarker.style.display === "none") {
+        lemurmarker.style.display = "block";
+    } else {
+        lemurmarker.style.display = "none";
+    }
+}
+
+function toggle5(){
+    var x = jaguarmarker
+    document.getElementById("jaguarmarker");
+    if (jaguarmarker.style.display === "none") {
+        jaguarmarker.style.display = "block";
+    } else {
+        jaguarmarker.style.display = "none";
+    }
+}
